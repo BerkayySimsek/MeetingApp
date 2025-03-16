8f606c9292641b6471d1a804af008c2c155a6a6f
BerkaySimsek <berkayysimsekk@gmail.com>
2025-03-16 20:36:12 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers;
+
+public class HomeController : Controller
+{
+    // localhost
+    // localhost/home
+    // // localhost/home/index
+    public IActionResult Index()
+    {
+        int clock = DateTime.Now.Hour;
+
+        //ViewBag.Hello = clock > 12 ? "İyi Günler" : "Günaydın";
+        //ViewBag.UserName = "Berkay";
+
+        ViewData["Hello"] = clock > 12 ? "İyi Günler" : "Günaydın";
+        ViewData["UserName"] = "Berkay";
+
+        var meetingInfo = new MeetingInfo()
+        {
+            Id = 1,
+            Location = "İstanbul, Haliç Kongre Merkezi",
+            Date = new DateTime(2026, 03, 03, 20, 0, 0),
+            NumberOfPeople = 100
+        };
+
+        return View(meetingInfo);
+    }
+}
---
+++ Controllers/MeetingController.cs
@@ -0,0 +1,32 @@
+﻿using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class MeetingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(UserInfo model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
